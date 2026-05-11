73fabb186eea1e7cd3e5f1a83506bd55ac57471a
leastfavorite <ariaschnei@gmail.com>
2026-05-11 16:45:01 -0500

    Implemented from_file()

--- include/CsvErrors.hh
+++ include/CsvErrors.hh
@@ -2,7 +2,6 @@
 
 #include "Field.hh"
 #include <format>
-#include <span>
 #include <sstream>
 #include <string>
 #include <string_view>
@@ -10,7 +9,11 @@
 #include <variant>
 
 // TODO: it might makes more sense to have .message() and its associate .err()s
-// supply an ostream << operator instead.
+// supply an ostream << operator instead? we're sending strings around which is
+// creating extraneous copies.
+
+// i don't expect this error type to get called a lot though, so it's kinda
+// moot
 
 template <typename CharT, typename T>
 consteval std::string __format_span(const T &s) {
@@ -40,7 +43,7 @@ struct CsvError {
 
     std::basic_string<atom_type> message() const {
         const auto err = std::visit([](const auto &e) {
-            return e.template err<Fs...>();
+            return e.err();
         }, error);
 
         const auto name = std::visit([]<typename T>(const T &)
@@ -57,64 +60,79 @@ struct CsvError {
 
 template <typename CharT>
 struct FileNotFound {
-    static constexpr std::basic_string<CharT> Name = "File not found";
-
-    template <is_field<CharT> ...Fs>
+    static constexpr auto Name = "File not found";
     std::basic_string<CharT> err() const { return "Could not open file."; }
 };
 
-template <typename CharT>
+template <typename CharT, is_field<CharT> ...Fs>
 struct UnknownHeader {
     static constexpr auto Name = "Unexpected header entry";
     std::vector<std::basic_string_view<CharT>> failures;
 
-    template <is_field<CharT> ...Fs>
     std::basic_string<CharT> err() const {
+        auto format_span = [](const auto &s) {
+            std::basic_stringstream<CharT> ss;
+            ss << '[';
+
+            for (size_t i = 0; i < s.size(); i++) {
+                if (i > 0) {
+                    ss << ", ";
+                }
+                ss << s[i];
+            }
+
+            ss << ']';
+
+            return ss.str();
+        };
+
         return std::format("Could not find header(s) {} in expected set {}",
-            __format_span(failures),
-            __format_span(FieldAnnotations<CharT, Fs...>));
+            format_span(failures),
+            format_span(FieldAnnotations<CharT, Fs...>));
     }
 };
 
-template <typename CharT>
+template <typename CharT, is_field<CharT> ...Fs>
 struct HeaderNotFound {
-    static constexpr std::basic_string<CharT> Name = "Required headers not found";
+    static constexpr auto Name = "Required headers not found";
 
-    std::vector<size_t> indices;
+    std::array<bool, sizeof...(Fs)> indices;
 
-    template <is_field<CharT> ...Fs>
     std::basic_string<CharT> err() const {
         std::basic_stringstream<CharT> ss;
-        for (size_t i = 0; i < indices.size(); i++) {
-            ss << "Could not find header '"
-               << FieldAnnotations<CharT, Fs...>[indices[i]];
-            if (i < indices.size() - 1) {
-                ss << "\n";
+
+        for (size_t i = 0; i < sizeof...(Fs); i++) {
+            if (!indices[i]) {
+                ss << "Could not find header '"
+                   << FieldAnnotations<CharT, Fs...>[i];
+                if (i < indices.size() - 1) {
+                    ss << "\n";
+                }
             }
         }
+
+        return ss.str();
     }
 
 };
 
-template <typename CharT>
+template <typename CharT, is_field<CharT> ...Fs>
 struct LengthMismatch {
     static constexpr auto Name = "Mismatched header length";
 
     size_t length;
 
-    template <is_field<CharT> ...Fs>
     std::basic_string<CharT> err() const {
         return std::format("Expected {} fields, got {}", sizeof...(Fs), length);
     }
 };
 
-template <typename CharT>
+template <typename CharT, is_field<CharT> ...Fs>
 struct CouldNotConvert {
     static constexpr auto Name = "Conversion Error";
 
     std::vector<std::pair<size_t, std::basic_string_view<CharT>>> failures;
 
-    template <is_field<CharT> ...Fs>
     std::basic_string<CharT> err() const {
 
         std::basic_stringstream<CharT> ss;
@@ -123,7 +141,7 @@ struct CouldNotConvert {
             const auto &[idx, found] = failures[i];
 
             ss << std::format("Could not convert field '{}': got '{}'",
-                    FieldAnnotations<Fs...>[idx], found);
+                    FieldAnnotations<CharT, Fs...>[idx], found);
             if (i < failures.size() - 1) {
                 ss << "\n";
             }
@@ -137,12 +155,17 @@ template<typename CharT, is_field<CharT> ...Fs>
 using FromFileError = CsvError<
         CharT,
         "from_file",
-        std::variant<FileNotFound<CharT>, UnknownHeader<CharT>, HeaderNotFound<CharT>>,
+        std::variant<
+            FileNotFound<CharT>,
+            UnknownHeader<CharT, Fs...>,
+            HeaderNotFound<CharT, Fs...>>,
         Fs...>;
 
 template<typename CharT, is_field<CharT> ...Fs>
 using DereferenceError = CsvError<
         CharT,
         "operator*",
-        std::variant<LengthMismatch<CharT>, CouldNotConvert<CharT>>,
+        std::variant<
+            LengthMismatch<CharT, Fs...>,
+            CouldNotConvert<CharT, Fs...>>,
         Fs...>;
--- src/main.cc
+++ src/main.cc
@@ -1,59 +1,37 @@
-#include "NamedTuple.hh"
+#include "CsvReader.hh"
 #include <iostream>
-#include "CsvErrors.hh"
 
-template<class CharT, class ...Fs>
-using FromFileError = CsvError<CharT, "from_file", std::variant<
-    FileNotFound<CharT>, LengthMismatch<CharT>, MismatchedHeader<CharT>>,
-Fs...>;
+int main(int argc, char *argv[]) {
 
-int main() {
-    auto n = NamedTuple<
-        char,
+    if (argc < 2) {
+        std::cerr << "Usage: csv <file.csv>" << std::endl;
+        return -1;
+    }
+
+    auto csv_result = CsvReader<
         Field<"symbol", std::string>,
-        Field<"test", double>
-    >();
+        Field<"venue", std::string>,
+        Field<"price", double>
+    >::from_file(argv[1]);
 
-    auto error = FromFileError<char,
-        Field<"symbol", std::string>,
-        Field<"test", double>
-    >();
-    std::cout << error.message() << std::endl;
+    if (!csv_result) {
+        std::cerr << csv_result.error().message() << std::endl;
+        return -1;
+    }
 
-    std::cout << n.get<"symbol">() << std::endl;
+    for (const auto &entry_result : **csv_result) {
+        if (!entry_result) {
+            std::cerr << "\n" << entry_result.error().message() << "\n" << std::endl;
+            continue;
+        }
+
+        // entry derives std::tuple<std::string, std::string, double>...
+        const auto &entry = *entry_result;
+
+        // ...with additional compile-time .get() to retrieve values by name
+        std::cout << std::format("symbol: '{}', venue: '{}', price * 2: {}",
+            entry.get<"symbol">(),
+            entry.get<"venue">(),
+            entry.get<"price">() * 2) << std::endl;
+    }
 }
-
-// int main(int argc, char *argv[]) {
-//
-//     if (argc < 2) {
-//         std::cerr << "Usage: csv <file.csv>" << std::endl;
-//         return -1;
-//     }
-//
-//     auto csv_result = CsvReader<char,
-//         Field<"symbol", std::string>,
-//         Field<"venue", std::string>,
-//         Field<"price", double>
-//     >::from_file(argv[1]);
-//
-//     if (!csv_result) {
-//         std::cerr << err_msg(csv_result) << std::endl;
-//         return -1;
-//     }
-//
-//     for (const auto &entry_result : **csv_result) {
-//         if (!entry_result) {
-//             std::cerr << "\n" << err_msg(entry_result) << "\n" << std::endl;
-//             continue;
-//         }
-//
-//         // entry derives std::tuple<std::string, std::string, double>...
-//         const auto &entry = *entry_result;
-//
-//         // ...with additional compile-time .get() to retrieve values by name
-//         std::cout << std::format("symbol: '{}', venue: '{}', price * 2: {}",
-//             entry.get<"symbol">(),
-//             entry.get<"venue">(),
-//             entry.get<"price">() * 2) << std::endl;
-//     }
-// }
